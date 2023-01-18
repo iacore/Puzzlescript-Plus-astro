881d1c89430fb7f8f967ab2d4ff931503de04ce5
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-18 11:15:33 +0000

    Move analysis scripts into separate folder

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 .PHONY: all
-all: defs.tsv undef2.tsv
+all: scripts/undef2.tsv scripts/defs.tsv
 
-defs.tsv: src/js/*.js
-	python search-function-var.py > defs.tsv
+scripts/defs.tsv: scripts/search-function-var.py src/js/*.js
+	python scripts/search-function-var.py > scripts/defs.tsv
 
-undef2.tsv: undef.txt
-	python match-undef.py > undef2.tsv
+scripts/undef2.tsv: scripts/match-undef.py scripts/undef.txt
+	python scripts/match-undef.py > scripts/undef2.tsv
 
-undef.txt: src/js/*.js
-	node_modules/.bin/eslint -f unix src/js/*.js | cat > undef.txt
+scripts/undef.txt: src/js/*.js
+	node_modules/.bin/eslint -f unix src/js/*.js > scripts/undef.txt
--- scripts/search-function-var.py
+++ scripts/search-function-var.py
@@ -4,14 +4,24 @@ r_let = re.compile(r"let ([_\w0-9]+)( =)?.*")
 r_const = re.compile(r"const ([_\w0-9]+)( =)?.*")
 r_func = re.compile(r"(?:async )?function ([_\w0-9]+).*")
 
-
 for jsfile in glob.glob('src/js/*'):
     with open(jsfile) as f:
         filename = os.path.basename(jsfile)
+        
         for i, line in enumerate(f):
-            match = r_var.match(line) or r_let.match(line) or r_const.match(line) or r_func.match(line)
+            match = None
+            reason = None
+            def try_match(r, what):
+                global match, reason
+                if not match:
+                    match = r.match(line)
+                    reason = what
+            try_match(r_var, "var")
+            try_match(r_let, "let")
+            try_match(r_const, "const")
+            try_match(r_func, "function")
             if match:
-                print(filename, match[1], i, match[0], sep='\t')
+                print(filename, match[1], i, reason, match[0], sep='\t')
     # print(
 # for line in open('undef.txt'):
 #     match = r.match(line)
